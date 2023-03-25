687ce60323a59298af900c4a7ee04ef76252b5d9
onefuncman <45834012+onefuncman@users.noreply.github.com>
2023-03-25 06:14:18 -0500

    gpt rewrite v1

---
+++ README.md
@@ -0,0 +1,5 @@
+# telnet proxy
+
+This is a telnet proxy currently for use to connect to a local port and proxy the connection locally to greatermud.com
+
+In the future, the proxy will be configurable to ship data externally for notifications and for custom item highlighting based on data lookup.
---
+++ main.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+from typing import List
+from enum import Enum
+
+class Direction(Enum):
+    ToMud = 1
+    FromMud = 2
+
+def begin_receive(buffer, direction, origin, destination, waiter):
+    def on_receive(result):
+        read = origin.recv(len(buffer))
+        if not read or not destination:
+            origin.close()
+            waiter.set()
+            return
+
+        destination.sendall(read)
+        origin.settimeout(0)
+        origin.recv_into(buffer)
+        origin.settimeout(None)
+
+    origin.settimeout(None)
+    origin.setblocking(True)
+    threading.Thread(target=origin.begin_recv, args=(buffer, 0, len(buffer), socket.MSG_WAITALL, on_receive, None)).start()
+
+async def begin(handler):
+    local_end_point = handler.getpeername()
+    proxy_address = "217.180.196.241" #"greatermud.com"
+    proxy_ip = socket.gethostbyname(proxy_address)
+    mud_end_point = (proxy_ip, 2427)
+
+    outbound = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    outbound.settimeout(5)
+    outbound.connect(mud_end_point)
+    outbound.setblocking(True)
+
+    waiter = threading.Event()
+    buffer = bytearray(1024)
+    begin_receive(buffer, Direction.ToMud, handler, outbound, waiter)
+    begin_receive(buffer, Direction.FromMud, outbound, handler, waiter)
+
+    await waiter.wait()
+    outbound.close()
+
+    print(f"closed connection from: {local_end_point}")
+
+async def start():
+    inbound = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    inbound.bind(('0.0.0.0', 1080))
+    inbound.listen(10)
+
+    sessions = []
+    while True:
+        handler, _ = await inbound.accept()
+        print(f"received connection from: {handler.getpeername()}")
+        sessions.append(begin(handler))
+
+if __name__ == "__main__":
+    asyncio.run(start())
+
